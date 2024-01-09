efd5b4ece1051b36ca48f7ae0e2e7c64bb045a2f
marticarbonell <marticarbonell11@gmail.com>
2024-01-09 19:36:16 +0100

    Iteration#4

--- js/basicAlgorithms/app.js
+++ js/basicAlgorithms/app.js
@@ -7,6 +7,7 @@
 //!------------------------------------------------------------------------------
 
 let myFavoritehero = "Hulk";
+
 console.log(myFavoritehero);
 
 //!------------------------------------------------------------------------------
@@ -78,6 +79,7 @@ const toy1 = { name: "Buss myYear", price: 19 };
 const toy2 = { name: "Rallo mcKing", price: 29 };
 
 let total = toy1.price + toy2.price;
+
 console.log(total);
 
 //!------------------------------------------------------------------------------
@@ -135,6 +137,7 @@ teniendo dos variables p = 10 y j = 5.
 let p = 10,
   j = 5;
 let o = p + j;
+
 console.log(o);
 
 //!------------------------------------------------------------------------------
@@ -146,4 +149,90 @@ teniendo dos variables c = 10 y m = 5.
 let c = 10,
   m = 5;
 let i = c * m;
+
 console.log(i);
+
+//!------------------------------------------------------------------------------
+
+//**Iteración #4: Arrays**
+
+//!------------------------------------------------------------------------------
+
+//!------------------------------------------------------------------------------
+/*1.1 Consigue el valor "HULK" del array de avengers y muestralo por consola.
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+*/
+//!------------------------------------------------------------------------------
+
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+
+console.log(avengers[0]);
+
+//!------------------------------------------------------------------------------
+/*1.2 Cambia el primer elemento de avengers a "IRONMAN"
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+*/
+//!------------------------------------------------------------------------------
+
+avengers[0] = "IRONMAN";
+
+//!------------------------------------------------------------------------------
+/*1.3 console numero de elementos en el array usando la propiedad correcta de Array.
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+*/
+//!------------------------------------------------------------------------------
+
+console.log(avengers.length);
+
+//!------------------------------------------------------------------------------
+/*1.4 Añade 2 elementos al array: "Morty" y "Summer".
+Muestra en consola el último personaje del array
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+*/
+//!------------------------------------------------------------------------------
+
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+
+rickAndMortyCharacters.push("Morty", "Summer");
+
+console.log(rickAndMortyCharacters[rickAndMortyCharacters.length - 1]);
+
+//!------------------------------------------------------------------------------
+/*1.5 Elimina el último elemento del array y muestra el primero y el último por consola.
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+*/
+//!------------------------------------------------------------------------------
+
+const rickAndMortyCharacters2 = [
+  "Rick",
+  "Beth",
+  "Jerry",
+  "Morty",
+  "Summer",
+  "Lapiz Lopez",
+];
+
+rickAndMortyCharacters2.pop();
+
+console.log(
+  rickAndMortyCharacters2[0],
+  rickAndMortyCharacters2[rickAndMortyCharacters2.length - 1]
+);
+
+//!------------------------------------------------------------------------------
+/*1.6 Elimina el segundo elemento del array y muestra el array por consola.
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+*/
+//!------------------------------------------------------------------------------
+
+const rickAndMortyCharacters3 = [
+  "Rick",
+  "Beth",
+  "Jerry",
+  "Morty",
+  "Summer",
+  "Lapiz Lopez",
+];
+
+rickAndMortyCharacters3.splice(1, 1);
+console.log(rickAndMortyCharacters3);
