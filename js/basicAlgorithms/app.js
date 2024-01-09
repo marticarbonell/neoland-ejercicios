7c5a484db23507d6d501128474bf5b41b8f1d229
marticarbonell <marticarbonell11@gmail.com>
2024-01-09 16:59:28 +0100

    x

--- js/basicAlgorithms/app.js
+++ js/basicAlgorithms/app.js
@@ -1,15 +1,6 @@
 //!------------------------------------------------------------------------------
 
-/*
-**Iteración #1: Variables**
-
-```bash
-1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
-1.2 Crea una variable llamada x, asigna el valor 50 a ella.
-1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10. 
-1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.
-```
- */
+//**Iteración #1: Variables**
 
 //!------------------------------------------------------------------------------
 //1.1 -----Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
@@ -39,33 +30,7 @@ let z = h + y;
 
 //!------------------------------------------------------------------------------
 
-/*
-Iteración #2: Variables avanzadas
-
-```jsx
-1.1 Dado el siguiente javascript, cambia el valor de la propiedad age a 25.
-const character = {name: 'Jack Sparrow', age: 10};
-
-1.2 Declara 3 variables con los nombres y valores siguientes
-	firstName = 'Jon';
-	lastName = 'Snow';
-	age = 24;
-Muestralos por consola de esta forma:
-	'Soy Jon Snow, tengo 24 años y me gustan los lobos.'
-
-1.3 Dado el siguiente javascript, imprime con un console.log la suma del precio de
-ambos juguetes.
-const toy1 = {name: 'Buss myYear', price: 19};
-const toy2 = {name: 'Rallo mcKing', price: 29};
-
-1.4 Dado el siguiente javascript, actualiza el valor de la variable globalBasePrice a 25000
-y actualiza la propiedad finalPrice de todos los coches con el valor de su propiedad
-basePrice más el valor de la variable globalBasePrice.
-let globalBasePrice = 10000;
-const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
-const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
-```
-*/
+//**Iteración #2: Variables avanzadas**
 
 //!------------------------------------------------------------------------------
 
@@ -76,8 +41,7 @@ const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
 const character = { name: "Jack Sparrow", age: 25 };
 
 //!------------------------------------------------------------------------------
-/*
-1.2 Declara 3 variables con los nombres y valores siguientes
+/*1.2 Declara 3 variables con los nombres y valores siguientes
 
 firstName = 'Jon';
 lastName = 'Snow';
@@ -102,8 +66,7 @@ console.log(
 );
 
 //!------------------------------------------------------------------------------
-/*
-1.3 Dado el siguiente javascript, imprime con un console.log la suma del precio de
+/*1.3 Dado el siguiente javascript, imprime con un console.log la suma del precio de
 ambos juguetes.
 
 const toy1 = {name: 'Buss myYear', price: 19};
@@ -118,8 +81,7 @@ let total = toy1.price + toy2.price;
 console.log(total);
 
 //!------------------------------------------------------------------------------
-/*
-1.4 Dado el siguiente javascript, actualiza el valor de la variable globalBasePrice a 25000
+/*1.4 Dado el siguiente javascript, actualiza el valor de la variable globalBasePrice a 25000
 y actualiza la propiedad finalPrice de todos los coches con el valor de su propiedad
 basePrice más el valor de la variable globalBasePrice.
 let globalBasePrice = 10000;
@@ -139,3 +101,13 @@ car1.finalPrice = globalBasePrice + car1.basePrice;
 car2.finalPrice = globalBasePrice + car2.basePrice;
 
 console.log(car1, car2);
+
+//!------------------------------------------------------------------------------
+
+//**Iteración #3: Operadores**
+
+//!------------------------------------------------------------------------------
+
+//!------------------------------------------------------------------------------
+//1.1 Multiplica 10 por 5 y muestra el resultado mediante console.
+//!------------------------------------------------------------------------------
